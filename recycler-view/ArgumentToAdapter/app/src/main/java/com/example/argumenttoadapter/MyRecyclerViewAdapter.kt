e94cf54c54a05d899b158891a5670fd08edfb5da
Kanmanemone <aa20438272@gmail.com>
2024-02-07 20:34:46 +0900

    RecyclerView - Adapter에 인자(Argument) 전달 (https://kenel.tistory.com/101)

---
+++ recycler-view/ArgumentToAdapter/app/src/main/java/com/example/argumenttoadapter/MyRecyclerViewAdapter.kt
@@ -0,0 +1,42 @@
+package com.example.argumenttoadapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class MyRecyclerViewAdapter(val menus: List<Menu>, val clickListener: (Menu) -> Unit) :
+    RecyclerView.Adapter<MyViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val listItem = layoutInflater.inflate(R.layout.list_item, parent, false)
+        return MyViewHolder(listItem)
+    }
+
+    override fun getItemCount(): Int {
+        return menus.size
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+/*
+        // 나쁘지 않지만 객체지향적이지는 않은 코드
+        val menu = menus[position]
+        holder.textItem.text = "${menu.name}\n${menu.price}₩"
+*/
+        // 객체지향적인 코드
+        holder.bind(menus[position], clickListener)
+    }
+}
+
+class MyViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+    val textItem: TextView = view.findViewById(R.id.textItem)
+
+    fun bind(menu: Menu, clickListener: (Menu) -> Unit) {
+        textItem.text = "${menu.name}\n${menu.price}₩"
+
+        view.setOnClickListener {
+            clickListener(menu)
+        }
+    }
+}
