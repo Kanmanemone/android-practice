fb419978ffb1f7429354eb84bb9935529a684546
Kanmanemone <aa20438272@gmail.com>
2024-06-17 20:00:36 +0900

    WorkManager - 기초 (https://kenel.tistory.com/149)

---
+++ work-manager/WorkManagerBasics/app/src/main/java/com/example/workmanagerbasics/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.example.workmanagerbasics
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.work.OneTimeWorkRequest
+import androidx.work.PeriodicWorkRequest
+import androidx.work.WorkManager
+import java.util.concurrent.TimeUnit
+
+lateinit var workManager: WorkManager
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        workManager = WorkManager.getInstance(applicationContext)
+
+        val oneTimeButton = findViewById<Button>(R.id.oneTimeButton)
+        val periodicButton = findViewById<Button>(R.id.periodicButton)
+
+        oneTimeButton.setOnClickListener {
+            setOneTimeWorkRequest()
+        }
+
+        periodicButton.setOnClickListener {
+            setPeriodicWorkRequest()
+        }
+    }
+
+    private fun setOneTimeWorkRequest() {
+        // WorkRequest 객체 만들기
+        val uploadRequest = OneTimeWorkRequest
+            .Builder(UploadWorker::class.java)
+            .build()
+
+        // WorkManager 객체의 작업 큐(Job queue)에 WorkRequest 등록
+        workManager.enqueue(uploadRequest)
+
+        // WorkInfo는 작업에 대한 정보를 담은 객체.
+        // WorkManager.getWorkInfoByIdLiveData()는 LiveData<WorkInfo>를 반환.
+        val workInfo = workManager.getWorkInfoByIdLiveData(uploadRequest.id)
+        workInfo.observe(this, Observer {
+            Log.i("interfacer_han", "(${this::class.simpleName}) UploadWorker's state: ${it.state.name}")
+        })
+    }
+
+    private fun setPeriodicWorkRequest() {
+        // WorkRequest 객체 만들기
+        val downloadRequest = PeriodicWorkRequest
+            .Builder(DownloadWorker::class.java, 15, TimeUnit.MINUTES) // 안드로이드에서 허용하는 PeriodicWorkRequest의 최소 주기는 15분이다.
+            .build()
+
+        // WorkManager 객체의 작업 큐(Job queue)에 WorkRequest 등록
+        workManager.enqueue(downloadRequest)
+
+        // WorkInfo는 작업에 대한 정보를 담은 객체.
+        // WorkManager.getWorkInfoByIdLiveData()는 LiveData<WorkInfo>를 반환.
+        val workInfo = workManager.getWorkInfoByIdLiveData(downloadRequest.id)
+        workInfo.observe(this, Observer {
+            Log.i("interfacer_han", "(${this::class.simpleName}) DownloadWorker's state: ${it.state.name}")
+        })
+    }
+}
